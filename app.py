b7b6ee4cea5359a2bd4fc292493ba44a19ab4d06
Arjun9271 <arjun.t1729@gmail.com>
2024-10-15 08:12:43 +0530

    add func

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/about')
 def home():
-    return 'Welcome To Future of AI'
+    return 'THIS IS HOME PAGE'
+
+@app.route('/magic')
+def add():
+    a = 2
+    b = 3
+    sum = a+b
+    return str(sum)
 
 
-if __name__ ==  '__main__':
-    app.run(debug = True)
-    
 
-
+if __name__ == '__main__':
+   app.run(debug = True)
